2bcf7f3cad4699b00e0d9586986e27f298c906ce
praveen <praveen@LAPTOP-HPC1MF24>
2020-05-22 18:58:13 +0530

    second commit

--- MagentoTesting/src/test/java/POM.java
+++ MagentoTesting/src/test/java/POM.java
@@ -3,7 +3,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class POM {
@@ -11,7 +11,7 @@ public class POM {
 	@Test
 	public void credential()
 	{
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
